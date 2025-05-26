cc303b8d10fb0c621ca5afe5ed1e14dc6148eca9
jaynouMa <jaynouma28@gmail.com>
2025-05-26 15:49:12 -0500

    like literally fixed nohtig

--- src/com/jaynou/rigidbodymk1/Vector3.java
+++ src/com/jaynou/rigidbodymk1/Vector3.java
@@ -18,4 +18,18 @@ public class Vector3 {
     public Vector3 multiply(double scalar) {
         return new Vector3(x * scalar, y * scalar, z * scalar);
     }
+
+    public double dot(Vector3 other) {
+        return x * other.x + y * other.y + z * other.z;
+    }
+
+    public double length() {
+        return Math.sqrt(x * x + y * y + z * z);
+    }
+
+    public Vector3 normalize() {
+        double len = length();
+        if (len == 0) return new Vector3(0, 1, 0);
+        return new Vector3(x / len, y / len, z / len);
+    }
 }
